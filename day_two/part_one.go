d054059039ab7708965cca2d07bfbf88760e031b
lb9203 <lb9203@student.uni-lj.si>
2020-12-07 21:07:33 +0100

    Completed day two, part one.

---
+++ day_two/part_one.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Read input file
+	file, err := os.Open("day_two/passwords.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var validPasswords = 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitLine := strings.Split(line, ":")
+
+		policy, password := splitLine[0], splitLine[1]
+
+		splitPolicy := strings.Split(policy, " ")
+
+		policyLetter := splitPolicy[1]
+
+		limits := strings.Split(splitPolicy[0], "-")
+
+		min, err := strconv.Atoi(limits[0])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		max, err := strconv.Atoi(limits[1])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		count := strings.Count(password, policyLetter)
+
+		if count <= max && count >= min {
+			validPasswords++
+		}
+
+	}
+
+	fmt.Printf("Number of valid passwords: %d\n", validPasswords)
+}
