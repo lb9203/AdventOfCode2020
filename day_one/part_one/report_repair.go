53793e187c97fe7f62eeaec1875c4f574d9b1ca5
lb9203 <lb9203@student.uni-lj.si>
2020-12-07 19:02:25 +0100

    Completed day one, part one

---
+++ day_one/part_one/report_repair.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	//Read input file
+	var file, err = os.Open("day_one/part_one/expense_report.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	//Parse input into int array
+	var expenseReport = map[int]int{}
+	var scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		var line = scanner.Text()
+
+		var lineAsInt, err = strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		expenseReport[lineAsInt] = lineAsInt
+
+		var matchingNumber = 2020 - lineAsInt
+		var _, exists = expenseReport[matchingNumber]
+
+		if exists {
+			fmt.Printf("My matching numbers are %d and %d, their product is %d.", lineAsInt, matchingNumber, lineAsInt*matchingNumber)
+			break
+		}
+	}
+
+}
